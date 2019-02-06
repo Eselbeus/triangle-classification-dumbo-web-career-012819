ec5ae2644493fb632b15d9eead290937cc700412
eselbeus <eselbeus@gmail.com>
2019-02-06 20:30:58 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -17,7 +17,7 @@ class Triangle
       rescue TriangleError => error
         puts error
       end
-    elsif @side1 <= 0 || @side2 <= 0 || @side3 <= 0
+    elsif @side1 <= 0 && @side2 <= 0 && @side3 <= 0
       begin
         raise TriangleError
       rescue TriangleError => error
