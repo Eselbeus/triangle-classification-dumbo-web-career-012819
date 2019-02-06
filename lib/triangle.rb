6f695808bcf8d00f0251d26d348882e79a1d7f39
eselbeus <eselbeus@gmail.com>
2019-02-06 21:13:19 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -20,8 +20,8 @@ class Triangle
     elsif @side1 <= 0 || @side2 <= 0 || @side3 <= 0
       begin
         raise TriangleError
-      rescue TriangleError => error
-        puts error
+      rescue TriangleError # => error
+        # puts error
       end
     elsif @side1 == @side2 && @side1 == @side3
       return :equilateral
