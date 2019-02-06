965017cd8ebed164318cdd52dc4837d0fbf6f964
eselbeus <eselbeus@gmail.com>
2019-02-06 20:12:34 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,7 +18,7 @@ class Triangle
     elsif @side1 == @side2 && @side1 == @side3
       return :equilateral
     elsif @side1 == @side2 || @side1 == @side3 || @side2 == @side3
-      return :isoceles
+      return :isosceles
     else
       return :scalene
     end
